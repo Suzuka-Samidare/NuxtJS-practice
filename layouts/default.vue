2bf4ec75f993fb90c0f94ac733fd1c0ac8658be2
Suzuka <saberlily031@gmail.com>
2020-07-23 03:18:25 +0900

    feat(common): first commit.

---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <h1 id="header">
+      Suzuka-Samidare Practice NuxtJS
+    </h1>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+#header {
+  background-color: rgb(0, 194, 194);
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+.button--cyan {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid rgb(1, 148, 148);
+  color: rgb(1, 148, 148);
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--cyan:hover {
+  color: #fff;
+  background-color: rgb(1, 148, 148);
+}
+
+.page-headline {
+  margin-bottom: 24px;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="margin: 20px;">
+    <h1 class="page-headline">
+      PROFILE
+    </h1>
+    <div style="margin: 20px; margin-bottom: 60px;">
+      <div class="row">
+        <h3>name</h3>
+        <div style="margin-left: 20px;">
+          さみだれ すずか
+        </div>
+      </div>
+      <div class="row">
+        <h3>favorite</h3>
+        <div style="margin-left: 20px;">
+          我那覇響（THE IDOLM@STER）
+        </div>
+      </div>
+    </div>
+
+    <nuxt-link
+      to="/"
+      tag="a"
+      class="button--grey"
+    >
+      back
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.row {
+  margin-bottom: 20px;
+}
+
+.button--grey {
+  margin: 0;
+}
+</style>
